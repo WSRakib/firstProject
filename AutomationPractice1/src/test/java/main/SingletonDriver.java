dc2bdc75ec999156c83c646ba0436766bf694990
Owner <Owner@192.168.0.204>
2020-07-27 22:24:59 -0400

    Our First Commit!

---
+++ AutomationPractice1/src/test/java/main/SingletonDriver.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SingletonDriver {
+
+	public SingletonDriver(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	private WebDriver driver;
+
+	public WebDriver getDriver() {
+		return driver;
+	}
+
+
+	public WebDriver openBrowser(String browser, String url){
+
+		if(browser.equalsIgnoreCase("Chrome")) {
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get("https://www.google.com/");
+			driver.navigate().to(url);
+		}
+		else if(browser.equalsIgnoreCase("FireFox")){
+			driver = new FirefoxDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get("https://www.google.com/");
+			driver.navigate().to(url);
+		}
+		else if(browser.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.manage().window().maximize();
+			driver.get("https://www.google.com/");
+			driver.navigate().to(url);
+		}
+		else {
+			System.out.println("Inorrect Driver!");
+		}
+		return driver;
+	}
+
+
+}
---
+++ AutomationPractice1/src/test/java/test/TestPurpous.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestPurpous {
+@Test
+public void testing() {
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.google.com");
+}
+}
